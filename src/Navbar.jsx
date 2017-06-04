6f4b6e04d052382792d5267a0bd85f70492cb005
vheimpel <vheimpel@gmail.com>
2017-06-04 17:46:35 +0000

    Add styles

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
-        <div>There are {this.props.userCountProp} users online</div>
+        <div className="alert">There are {this.props.userCountProp} users online</div>
       </nav>
     );
   }
